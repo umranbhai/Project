2dd48e4ded165d5ad27158a6cd3537c28d607944
umranbhai <shaikhnabil230@gmail.com>
2025-06-21 11:06:33 +0530

    change

--- Todo/app.js
+++ Todo/app.js
@@ -3,15 +3,8 @@ const todoList = []
 
 addBtn.addEventListener("click", function () {
     const todoInput = document.getElementById('todoInput').value;
-    // document.getElementById("todoList").innerHTML = `<ul id='todoItems'>${todoInput}</ul>`;
-    todoList.push(todoInput);
-    const li = document.getElementById("list");
-    for (let i = 0; i < todoList.length; i++) {
-        // li.append()todoList[];
-        // console.log(todoList);
+    if (todoInput.trim() === "") {
+        alert("Please enter a todo item.");
+        return;
     }
-    // console.log(todoList)
-
 });
-`````````
-// console.log(addBtn)
